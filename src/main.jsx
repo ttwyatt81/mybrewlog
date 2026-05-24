b5afe835e8759d91da39954cf0ec5c9fecf5059b
ttwyatt81 <ttwyatt@outlook.com>
2026-05-24 16:21:12 +0200

    Update main.jsx
    Changed code to: <React.StrictMode><ErrorBoundary><App /></ErrorBoundary></React.StrictMode>

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import ErrorBoundary from "./components/ErrorBoundary";
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode><ErrorBoundary><App /></ErrorBoundary></React.StrictMode>
 )
